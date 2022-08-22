44d5496c6369ac60f1e2a1548c3947f9f668ca2c
ohmjeemin <ohmjeemin@mobility42.io>
2022-08-22 22:05:50 +0900

    calculator test code

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        Calculator cal = new Calculator();
+        System.out.println(cal.add(3,4));
+        System.out.println(cal.subtract(5,4));
+        System.out.println(cal.multiply(2,3));
+        System.out.println(cal.divide(10,2));
+    }
+}
---
+++ test/CalculatorTest.java
@@ -0,0 +1,22 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CalculatorTest {
+
+    @Test
+    void add() {
+    }
+
+    @Test
+    void subtract() {
+    }
+
+    @Test
+    void multiply() {
+    }
+
+    @Test
+    void divide() {
+    }
+}
